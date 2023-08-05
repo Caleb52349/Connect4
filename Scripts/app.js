46d5662d5066c0809f78ff32726e4a78d87fa4e9
Caleb Danso <calebklasnic@yahoo.com>
2023-08-05 18:09:48 +0200

    Element created for app.js
    Essential variable created to be accessed and behave  via click on a button on the main menu

---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+//Elements
+const mainMenu = document.querySelector('.main-menu');
+const gameContainer = document.querySelector('.game-container');
+const rulesContainer = document.querySelector('.rules-container');
+const gameMenuBtn = document.querySelector('.game-menu');
+const vsPlayerBtn = document.getElementById('vs-player');
+const gameRulesBtn = document.getElementById('game-rules');
+const menuBtn = document.getElementById('menu');
